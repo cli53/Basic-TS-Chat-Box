39752c6f9d14b7126e4ef83518b1e8debd75cf56
LI, CHRIS <chrisli@northwesternmutual.com>
2020-04-21 14:41:15 -0400

    feat(style): adding styled components to chat message and container

--- my-app/src/components/chat_container/index.jsx
+++ my-app/src/components/chat_container/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import { useFetch } from "../../hooks";
 import PropTypes from "prop-types";
 import ChatInput from "../chat_input";
 import ChatMessages from "../chat_messages";
+
+const ChatTitle = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const ChatContainer = ({ user }) => {
   const url = "https://api.jsonbin.io/b/5e9a6b452940c704e1da618a";
   const { data: messages, isLoading } = useFetch(url);
   return (
     <div>
-      <h1>Stuck at Home Group Chat</h1>
+      <ChatTitle>Stuck at Home Group Chat</ChatTitle>
       <ChatMessages messages={messages} isLoading={isLoading} user={user} />
       <ChatInput />
     </div>
@@ -17,7 +24,7 @@ const ChatContainer = ({ user }) => {
 };
 
 ChatContainer.propTypes = {
-  user: PropTypes.string
+  user: PropTypes.object
 };
 
 export default ChatContainer;
--- my-app/src/components/chat_messages/index.jsx
+++ my-app/src/components/chat_messages/index.jsx
@@ -1,101 +1,72 @@
 import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
-import { map, filter, isEmpty } from "lodash";
-import { getTimeStamp, interleavingMessages } from "./utils";
+import { map, isEmpty } from "lodash";
+import { interleavingMessages } from "./utils";
 import LoadingIndicator from "../chat_loading";
 
 // TODO: implement scroll to bottom functionality
 // TODO: implement rendering emojis
+// bubble, text, avatar, details, displayName
+
+const DisplayName = styled.header`
+  font-weight: 600;
+`;
+
+const TimeDetails = styled.p`
+  font-size: 0.5rem;
+  color: lightgrey;
+`;
+
+const ChatBubble = styled.div`
+  height: fit-content;
+  width: fit-content;
+  padding: 5px;
+  border-radius: 2%;
+  background-color: lightblue;
+  color: black;
+`;
+
+const Avatar = styled.div`
+  border-radius: 50%;
+  height: 75px;
+  width: 75px;
+`;
+
+const ChatRow = styled.section`
+  display: flex;
+`;
 
 const ChatMessages = ({ messages = [], isLoading, user: loginUser = {} }) => {
   if (isLoading) return <LoadingIndicator />;
-  const messageTypes = {
-    CONNECT: "connect",
-    DISCONNECT: "disconnect",
-    MESSAGE: "message",
-    UPDATE: "update",
-    DELETE: "delete"
-  };
+
   if (!isLoading && !isEmpty(messages)) {
-    const filteredMessages = interleavingMessages(messages);
-    const formattedMessages = map(
-      filteredMessages,
-      (
-        {
-          delta,
-          payload: {
-            type,
-            user: { id: userId, display_name } = {},
-            message: { id: messageId = 0, text } = {}
-          }
-        },
-        key
-      ) => {
-        const time = getTimeStamp(delta);
-        if (type === messageTypes.MESSAGE) {
-          return { time, display_name, text, messageId, userId, key };
-        } else if (type === messageTypes.CONNECT) {
-          return {
-            time,
-            display_name,
-            text: `${display_name} has joined the chat`,
-            userId,
-            key
-          };
-        } else if (type === messageTypes.DISCONNECT) {
-          return {
-            time,
-            display_name,
-            text: `${display_name} has left the chat`,
-            userId,
-            key
-          };
-        } else if (type === messageTypes.DELETE) {
-          return { messageId, time, text: "deleted", key };
-        } else if (type === messageTypes.UPDATE) {
-          console.log(text);
-          return { time, text, key };
-        }
-      }
-    );
+    const formattedMessages = interleavingMessages(messages);
     return map(formattedMessages, message => {
-      let isUser =
-        !isEmpty(message) &&
-        message.userId === !isEmpty(message) &&
-        loginUser.id;
+      let isUser = message.userId === loginUser.id;
       let renderName = message?.display_name;
       return (
-        <div
-          key={message.key}
-          className="chat-bubble-row"
-          style={{ flexDirection: isUser ? "row-reverse" : "row" }}
-        >
-          {/* <img
-            src={message.sender.avatar}
-            alt="sender avatar"
-            className="avatar"
-            style={isUser ? { marginLeft: "15px" } : { marginRight: "15px" }}
-          /> */}
-          <div className={`chat-bubble`} key={message.id}>
-            {renderName} {message?.time}
-            <div
-              className="message"
-              style={{ color: isUser ? "#FFF" : "#2D313F" }}
-            >
-              <span>{message.text}</span>
-            </div>
-          </div>
-        </div>
+        <ChatRow className={`chat-row`} key={message.id}>
+          <Avatar alt="sender avatar" className="avatar">
+            {message.avatar}
+          </Avatar>
+          <ChatBubble>
+            <DisplayName>{renderName}</DisplayName>
+            {message.text}
+          </ChatBubble>
+          <TimeDetails>{message.time}</TimeDetails>
+        </ChatRow>
       );
     });
   } else {
     return <h1>messages</h1>;
   }
 };
+
 ChatMessages.propTypes = {
   messages: PropTypes.array,
   isLoading: PropTypes.bool,
-  user: PropTypes.number
+  user: PropTypes.object
 };
 
 export default ChatMessages;
