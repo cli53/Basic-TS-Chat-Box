707c8ca5d243133eb94b555245f46dd79461fd50
LI, CHRIS <chrisli@northwesternmutual.com>
2020-04-22 18:04:21 -0400

    test(app): adding simple RTL tests

--- chat-replay/src/App.test.js
+++ chat-replay/src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { first, last, capitalize } from "lodash";
+import { render } from "@testing-library/react";
+import mockData from "./mock.json";
+import ChatMessages from "../src/components/chat_messages";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Renders empty message notification", () => {
+  const { getByText, queryByText } = render(
+    <ChatMessages messages={[]} user={{ id: 1 }} isLoading={true} />
+  );
+  const emptyMessage = "Nothing to see here...";
+  expect(getByText(emptyMessage)).toBeInTheDocument();
+  expect(queryByText("Something to see here")).not.toBeInTheDocument();
+});
+
+test("Renders initials for avatar", () => {
+  const { getAllByText } = render(
+    <ChatMessages messages={mockData} user={{ id: 1 }} isLoading={false} />
+  );
+  const avatarIntitals = capitalize(
+    `${first(mockData[0].payload.user.display_name)}${last(
+      mockData[0].payload.user.display_name
+    )}`
+  );
+
+  expect(first(getAllByText(avatarIntitals))).toBeInTheDocument();
+});
+
+test("Renders chat bubble with message", () => {
+  const { getByText } = render(
+    <ChatMessages messages={mockData} user={{ id: 1 }} isLoading={false} />
+  );
+  const firstChatBubbleMessage = mockData[0].payload.message.text;
+  expect(getByText(firstChatBubbleMessage)).toBeInTheDocument();
 });
