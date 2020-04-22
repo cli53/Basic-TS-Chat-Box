79c5d8799b55d4f281270563695d8e3cd7847d20
LI, CHRIS <chrisli@northwesternmutual.com>
2020-04-22 18:04:46 -0400

    chore(cleaning): removing user object from app component

--- chat-replay/src/App.js
+++ chat-replay/src/App.js
@@ -3,12 +3,9 @@ import "./App.css";
 import ChatContainer from "./components/chat_container";
 
 function App() {
-  const user = {
-    id: 1
-  };
   return (
     <div className="App">
-      <ChatContainer user={user} />
+      <ChatContainer />
     </div>
   );
 }
--- chat-replay/src/hooks.js
+++ chat-replay/src/hooks.js
@@ -7,7 +7,10 @@ export const useFetch = url => {
     setState(state => ({ data: state.data, isLoading: true }));
     fetch(url, {
       headers: {
-        "secret-key": ""
+        "secret-key":
+          "$2b$10$eMFpsgnOHYHl9JKinJ2oCOe51Jacavyw3Pe0En3HFBKTfGWcdM6qi"
+        // NOTE: I wouldn't normally push the API key and would just have it stored in a .env file
+        // process.env.REACT_APP_API_KEY
       }
     })
       .then(y => y.json())
@@ -15,5 +18,6 @@ export const useFetch = url => {
         setState({ data: x, isLoading: false });
       });
   }, [setState, url]);
+
   return state;
 };
