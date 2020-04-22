12936f908c5dbff2f939adadfe0e705a1a24728e
LI, CHRIS <chrisli@northwesternmutual.com>
2020-04-22 15:43:04 -0400

    feat(loading): adding loading component

---
+++ chat-replay/src/components/chat_loading/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../../logo.svg";
+
+const LoadingIndicator = () => {
+  return <img src={logo} className="App-logo" alt="logo" />;
+};
+
+export default LoadingIndicator;
