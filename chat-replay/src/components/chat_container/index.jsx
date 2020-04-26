cabc211e9c9075e3731050e828b247e8888388ed
LI, CHRIS <chrisli@northwesternmutual.com>
2020-04-26 16:32:04 -0400

    feat(scroll-bottom): implement smooth scroll to bottom

--- chat-replay/src/components/chat_container/index.jsx
+++ chat-replay/src/components/chat_container/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { useFetch } from "../../hooks";
-import { toNumber, last } from "lodash";
+import { toNumber } from "lodash";
 import ChatMessages from "../chat_messages";
 import ChatInput from "../chat_input";
 import LoadingIndicator from "../chat_loading";
@@ -13,6 +13,19 @@ const ChatTitle = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: palevioletred;
+`;
+
+const ScrollBottomButton = styled.button`
+  display: inline-block;
+  height: 1.5rem;
+  width: fit-content;
+  background-color: lightblue;
+  color: black;
+  border: none;
+  border-radius: 2px;
+  &::after {
+    content: " ⬇️";
+  }
 `;
 
 const ChatContainer = () => {
@@ -28,6 +41,11 @@ const ChatContainer = () => {
 
   const handleChange = event => {
     setCurrentUser(toNumber(event.target.value));
+  };
+
+  const scrollToBottom = () => {
+    const chatInput = document.getElementById("chat-input");
+    chatInput.scrollIntoView();
   };
 
   const updateMessages = value => {
@@ -48,8 +66,14 @@ const ChatContainer = () => {
         currentUser={currentUser}
       />
       <ChatTitle>Stuck at Home Group Chat</ChatTitle>
+      <ScrollBottomButton onClick={scrollToBottom}>
+        Click to Scroll Bottom
+      </ScrollBottomButton>
       <ChatMessages messages={formattedMessages} currentUser={currentUser} />
-      <ChatInput updateMessages={updateMessages} />
+      <ChatInput
+        updateMessages={updateMessages}
+        scrollToBottom={scrollToBottom}
+      />
     </>
   );
 };
